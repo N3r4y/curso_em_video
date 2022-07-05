a07d7aecfbee882a0209e917db2b1b2f5e129894
N3r4y <frafaelnery@gmail.com>
2022-07-05 19:50:27 -0300

    Initial commit

---
+++ Primeiros Passos/Desafio 03.py
@@ -0,0 +1,6 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+
+print('A some é', num1 + num2)
